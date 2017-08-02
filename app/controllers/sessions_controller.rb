b087b692e77bc38803aae89eb0a5db6a8e8befe6
kayaneuh <n.kaliane@gmail.com>
2017-08-02 14:25:58 +0200

    jusqu'au 11.2 success

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+=begin Pareil que :
+    if params[:session][:remember_me] == '1'
+    remember(user)
+    else
+    forget(user)
+    end
+=end
+      redirect_back_or user
+    else
+flash.now[:danger] = "Invalid email/password combination" #.now = le flash disparait la page suivante
+    render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,61 @@
+module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id #envoi un cookie temporaire pour garder connecté l'utilisateur, et est détruit lorsqu'il quitte
+  end
+
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id #genere cookie pour l'id
+    cookies.permanent[:remember_token] = user.remember_token #genere cookie pour le token
+  end
+
+  # Returns the user corresponding to the remember token cookie.
+ def current_user
+     if (user_id = session[:user_id]) #verifie si l'utilisateur s'est connecté
+       @current_user ||= User.find_by(id: user_id)
+     elsif (user_id = cookies.signed[:user_id]) #vérifie si l'utilisateur a des cookies permettant l'authentification
+       user = User.find_by(id: user_id) #compare les cookies à la BDD
+       if user && user.authenticated?(cookies[:remember_token]) #si l'utilisateur existe en que l'authentification avec le token crypté de notre BDD
+         log_in user
+         @current_user = user
+       end
+     end
+   end
+
+   # Returns true if the given user is the current user.
+ def current_user?(user)
+   user == current_user
+ end
+
+  def logged_in?
+   !current_user.nil?
+ end
+
+ # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+ # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+end

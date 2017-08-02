8eccccafc66ef34dd67c225420e9228ed387e3e8
kayaneuh <n.kaliane@gmail.com>
2017-08-02 14:43:55 +0200

    jusqu'au 11.2.2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,6 +16,11 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
+  def account_activation(user)
+   @user = user
+   mail to: user.email, subject: "Account activation"
+ end
+
   def password_reset
     @greeting = "Hi"
 

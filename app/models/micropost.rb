00f22e847c04985f7f9d93663828ad42b2db3921
kayaneuh <n.kaliane@gmail.com>
2017-08-03 14:53:05 +0200

    dependant story 13.1.4

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
-class Micropost < ApplicationRecord
+class Micropost < ActiveRecord::Base
   belongs_to :user
+  # Mettre les articles postés dans l'ordre inversé du plus récent au plus vieux. desc veut dire descendant
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end

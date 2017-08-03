06406849e261d1d6bc32eef5cbc2fcb8f377bc28
kayaneuh <n.kaliane@gmail.com>
2017-08-03 16:42:39 +0200

    13.3.4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    #si le user est connecté il peut créer un post
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
--- app/models/user.rb
+++ app/models/user.rb
@@ -74,6 +74,13 @@ class User < ApplicationRecord
    reset_sent_at < 2.hours.ago
  end
 
+ # Defines a proto-feed.
+ # See "Following users" for the full implementation.
+ #selectionne tous les posts du user
+ def feed
+   Micropost.where("user_id = ?", id)
+ end
+
   private
 
       # Converts email to all lower-case.

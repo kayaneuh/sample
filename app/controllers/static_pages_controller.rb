d7daeb63c85dc318d48e67887f4cfcd206d73ed1
kayaneuh <n.kaliane@gmail.com>
2017-08-03 16:27:42 +0200

    13.3.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    #si le user est connecté il peut créer un post
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help

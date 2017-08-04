a0753dbb061554214a989caaafafe3ba7bd8d9fb
kayaneuh <n.kaliane@gmail.com>
2017-08-04 11:09:54 +0200

    jusqu'au 14.1.4 followed users

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,7 @@ class Relationship < ApplicationRecord
   #les relationship ont des follower et des followed.
   belongs_to :follower, class_name: "User"
  belongs_to :followed, class_name: "User"
+ #pour valider les tests dans test/models/relationship_test.rb follower_id et followed_id doivent être présents
+ validates :follower_id, presence: true
+validates :followed_id, presence: true
 end
